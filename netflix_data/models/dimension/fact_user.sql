846808e3debbd90ac9188ee2f6e5c643d205c3a3
Adam Worede <adamg2609@gmail.com>
2025-07-25 14:39:23 +0100

    surrogate key added

--- netflix_data/models/dimension/fact_user.sql
+++ netflix_data/models/dimension/fact_user.sql
@@ -3,13 +3,25 @@ WITH tag_table AS (
         user_id,
         movie_id
     FROM {{ ref("stg_tags")}}
-)
+),
 
-WITH ratings_table AS (
+ratings_table AS (
     SELECT
         user_id,
         movie_id
     FROM {{ ref("staging") }}
-)
+),
 
+movie_user_pairs AS (
+    SELECT *
+    FROM tag_table
+    UNION
+    SELECT *
+    FROM ratings_table
+)
 -- get all the distinct pairs of movies and users, create a key pair 
+SELECT
+    user_id,
+    movie_id,
+     {{ dbt_utils.generate_surrogate_key(['user_id', 'movie_id']) }} AS user_movie_id
+FROM 
