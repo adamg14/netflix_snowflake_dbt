75f3f62642b3260913669bb7a0ef813a954c3aee
Adam Worede <adamg2609@gmail.com>
2025-07-27 14:45:35 +0100

    staging table generic tests complete

--- netflix_data/models/staging/stg_links.sql
+++ netflix_data/models/staging/stg_links.sql
@@ -4,13 +4,22 @@ WITH raw_links AS (
 ),
 
 unique_movies AS (
-    SELECT DISTINCT movie_id,
-     imdb_id, tmdb_id
+    SELECT
+        DISTINCT movie_id, imdb_id, tmdb_id
      FROM raw_links
+),
+
+ranked_rows AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER(PARTITION BY tmdb_id ORDER BY movie_id) AS ranking_movies
+    FROM unique_movies
 )
 
 SELECT
     movie_id,
     imdb_id,
     tmdb_id
-FROM unique_movies
+FROM ranked_rows
+WHERE ranking_movies = 1
+AND tmdb_id IS NOT NULL
